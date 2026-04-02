299156a8f3018363b69fa5683d4583173dbd30b5
pgonia8-wq <pgonia8@gmail.com>
2026-04-01 18:10:47 -0600

    Update main.tsx

--- artifacts/hfans/src/main.tsx
+++ artifacts/hfans/src/main.tsx
@@ -2,4 +2,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// 🔥 IMPORTANTE
+import { setBaseUrl } from "@workspace/api-client-react";
+
+// 👉 TU BACKEND REAL (CAMBIA SI ES NECESARIO)
+setBaseUrl("https://hfan1.onrender.com");
+
 createRoot(document.getElementById("root")!).render(<App />);
